d6985e0d74df1872a9e1dcf98aff1cf0a06428e3
Ryan Mehri <ryan.mehri1@gmail.com>
2021-10-22 13:01:50 -0700

    feat: update lualine theme to add more color

--- lua/lualine/themes/onenord.lua
+++ lua/lualine/themes/onenord.lua
@@ -1,20 +1,37 @@
 local colors = require("onenord.colors")
 
-local onenord = {
-	normal = {
-		a = { fg = colors.fg, bg = colors.blue, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.selection },
-		c = { fg = colors.fg, bg = colors.active },
-	},
-	insert = { a = { fg = colors.fg, bg = colors.green, gui = "bold" } },
-	visual = { a = { fg = colors.fg, bg = colors.purple, gui = "bold" } },
-	command = { a = { fg = colors.fg, bg = colors.yellow, gui = "bold" } },
-	replace = { a = { fg = colors.fg, bg = colors.red, gui = "bold" } },
-	inactive = {
-		a = { fg = colors.light_gray, bg = colors.highlight, gui = "bold" },
-		b = { fg = colors.light_gray },
-		c = { fg = colors.light_gray, bg = colors.active },
-	},
+local onenord = {}
+
+onenord.normal = {
+	a = { fg = colors.fg, bg = colors.cyan, gui = "bold" },
+	b = { fg = colors.cyan, bg = colors.highlight },
+	c = { fg = colors.fg, bg = colors.active },
+}
+
+onenord.insert = {
+	a = { fg = colors.fg, bg = colors.green, gui = "bold" },
+	b = { fg = colors.green, bg = colors.highlight },
+}
+
+onenord.command = {
+	a = { fg = colors.fg, bg = colors.yellow, gui = "bold" },
+	b = { fg = colors.yellow, bg = colors.highlight },
+}
+
+onenord.visual = {
+	a = { fg = colors.fg, bg = colors.purple, gui = "bold" },
+	b = { fg = colors.purple, bg = colors.highlight },
+}
+
+onenord.replace = {
+	a = { fg = colors.fg, bg = colors.red, gui = "bold" },
+	b = { fg = colors.red, bg = colors.highlight },
+}
+
+onenord.inactive = {
+	a = { fg = colors.light_gray, bg = colors.floating, gui = "bold" },
+	b = { fg = colors.light_gray, bg = colors.floating },
+	c = { fg = colors.light_gray, bg = colors.active },
 }
 
 return onenord
