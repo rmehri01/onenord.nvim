116ee93674fb556846717048d8bba7f7a6e8961e
Ryan Mehri <ryan.mehri1@gmail.com>
2021-11-01 10:23:17 -0700

    fix: increase lualine contrast #3

--- lua/lualine/themes/onenord.lua
+++ lua/lualine/themes/onenord.lua
@@ -3,28 +3,28 @@ local colors = require("onenord.colors")
 local onenord = {}
 
 onenord.normal = {
-  a = { fg = colors.fg, bg = colors.cyan, gui = "bold" },
+  a = { fg = colors.bg, bg = colors.cyan, gui = "bold" },
   b = { fg = colors.cyan, bg = colors.highlight },
   c = { fg = colors.fg, bg = colors.active },
 }
 
 onenord.insert = {
-  a = { fg = colors.fg, bg = colors.green, gui = "bold" },
+  a = { fg = colors.bg, bg = colors.green, gui = "bold" },
   b = { fg = colors.green, bg = colors.highlight },
 }
 
 onenord.command = {
-  a = { fg = colors.fg, bg = colors.yellow, gui = "bold" },
+  a = { fg = colors.bg, bg = colors.yellow, gui = "bold" },
   b = { fg = colors.yellow, bg = colors.highlight },
 }
 
 onenord.visual = {
-  a = { fg = colors.fg, bg = colors.purple, gui = "bold" },
+  a = { fg = colors.bg, bg = colors.purple, gui = "bold" },
   b = { fg = colors.purple, bg = colors.highlight },
 }
 
 onenord.replace = {
-  a = { fg = colors.fg, bg = colors.red, gui = "bold" },
+  a = { fg = colors.bg, bg = colors.red, gui = "bold" },
   b = { fg = colors.red, bg = colors.highlight },
 }
 
