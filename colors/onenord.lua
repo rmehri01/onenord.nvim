9b6efc573e1085c7b47dc881dd309e291bd765d5
Ryan Mehri <ryan.mehri1@gmail.com>
2021-11-25 15:22:12 -0800

    perf: enable caching when not in debug mode

--- colors/onenord.lua
+++ colors/onenord.lua
@@ -1,6 +1,9 @@
-package.loaded["onenord"] = nil
-package.loaded["onenord.util"] = nil
-package.loaded["onenord.colors"] = nil
-package.loaded["onenord.theme"] = nil
+-- Refresh cache for local debug and development purposes
+if vim.g.onenord_debug == true then
+  package.loaded["onenord"] = nil
+  package.loaded["onenord.util"] = nil
+  package.loaded["onenord.colors"] = nil
+  package.loaded["onenord.theme"] = nil
+end
 
 require("onenord.util").load()
