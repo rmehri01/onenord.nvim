08577c696ce1abf5c6fbc1a4a00ad851b5b51a38
Ryan Mehri <ryan.mehri1@gmail.com>
2021-12-06 09:09:17 -0800

    fix: adjust light mode colors to have more contrast

--- colors/onenord.lua
+++ colors/onenord.lua
@@ -3,6 +3,8 @@ if vim.g.onenord_debug == true then
   package.loaded["onenord"] = nil
   package.loaded["onenord.util"] = nil
   package.loaded["onenord.colors"] = nil
+  package.loaded["onenord.colors.onenord"] = nil
+  package.loaded["onenord.colors.onenordlight"] = nil
   package.loaded["onenord.theme"] = nil
 end
 
