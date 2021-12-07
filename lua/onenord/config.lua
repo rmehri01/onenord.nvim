234add286fea3fbf370293fc9e2a5131a55e63af
Ryan Mehri <ryan.mehri1@gmail.com>
2021-12-07 15:14:13 -0800

    feat: add ability to change styles without changing colors #17

--- lua/onenord/config.lua
+++ lua/onenord/config.lua
@@ -17,6 +17,7 @@ local defaults = {
     eob_lines = true, -- Hide the end-of-buffer lines
   },
   custom_highlights = {}, -- Overwrite default highlight groups
+  custom_styles = {}, -- Overwrite default styles for highlight groups
   custom_colors = {}, -- Overwrite default colors
 }
 
