71bdcdf105a8feb16cd5248b40e44c88832f75ca
Ryan Mehri <ryan.mehri1@gmail.com>
2023-02-05 21:50:46 -0800

    fix: make base syntax more consistent, works better with semantic tokens

--- lua/onenord/theme.lua
+++ lua/onenord/theme.lua
@@ -14,9 +14,9 @@ function theme.highlights(colors, config)
       -- static, register, volatile, etc.
       StorageClass = { fg = colors.purple },
       -- struct, union, enum, etc.
-      Structure = { fg = colors.purple },
+      Structure = { fg = colors.yellow },
       -- any constant
-      Constant = { fg = colors.blue },
+      Constant = { fg = colors.cyan },
       -- any character constant: 'c', '\n'
       Character = { fg = colors.green },
       -- a number constant: 5
@@ -34,9 +34,9 @@ function theme.highlights(colors, config)
       -- try, catch, throw
       Exception = { fg = colors.purple },
       -- generic Preprocessor
-      PreProc = { fg = colors.purple },
+      PreProc = { fg = colors.yellow },
       -- preprocessor #include
-      Include = { fg = colors.blue },
+      Include = { fg = colors.yellow },
       -- preprocessor #define
       Define = { fg = colors.red },
       -- same as Define
@@ -317,7 +317,7 @@ function theme.highlights(colors, config)
 
     local treesitter = {
       -- Annotations that can be attached to the code to denote some kind of meta information. e.g. C++/Dart attributes.
-      ["@attribute"] = { fg = colors.purple },
+      ["@attribute"] = { fg = colors.yellow },
       -- Boolean literals: `True` and `False` in Python.
       ["@boolean"] = { fg = colors.orange },
       -- Character literals: `'a'` in C.
@@ -348,7 +348,7 @@ function theme.highlights(colors, config)
       -- Built-in functions: `print` in Lua.
       ["@function.builtin"] = { fg = colors.cyan, style = config.styles.functions },
       -- Macro defined functions (calls and definitions): each `macro_rules` in Rust.
-      ["@function.macro"] = { fg = colors.blue },
+      ["@function.macro"] = { fg = colors.red },
       -- File or module inclusion keywords: `#include` in C, `use` or `extern crate` in Rust.
       ["@include"] = { fg = colors.blue },
       -- Keywords that don't fit into other categories.
